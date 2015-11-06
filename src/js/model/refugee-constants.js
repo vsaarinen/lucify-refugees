c31013e4d97940e10612a3cc9f8f23ad3632aa56
Ville Saarinen <ville.saarinen@gmail.com>
2015-11-06 22:27:02 +0200

    Start in 2011
    Also show more country labels

--- src/js/model/refugee-constants.js
+++ src/js/model/refugee-constants.js
@@ -3,16 +3,16 @@ var moment = require('moment');
 
 // note that month indices are zero-based
 
-module.exports.DATA_START_YEAR = 2012;
-module.exports.DATA_START_MONTH = 0; 
+module.exports.DATA_START_YEAR = 2011;
+module.exports.DATA_START_MONTH = 0;
 
 module.exports.DATA_END_YEAR = 2015;
 module.exports.DATA_END_MONTH = 8;
 
 module.exports.DATA_END_MOMENT = moment([
-	module.exports.DATA_END_YEAR, 
-	module.exports.DATA_END_MONTH, 30]); 
+	module.exports.DATA_END_YEAR,
+	module.exports.DATA_END_MONTH, 30]);
 
-module.exports.disableLabels = ['BIH', 'MKD', 'ALB', 'LUX', 'MNE', 'ARM', 'AZE', 'LBN'];
+module.exports.disableLabels = [];
 
-module.exports.labelShowBreakPoint = 992;
+module.exports.labelShowBreakPoint = 600;
